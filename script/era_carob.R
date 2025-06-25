32c36ced2533d6f3f5aca56d561dd20c63bd9edc
cedric Ngakou <cedric.ngakou@aims-cameroon.org>
2025-06-25 18:00:57 +0100

    imp

--- script/era_carob.R
+++ script/era_carob.R
@@ -12,6 +12,7 @@ r1 <- do.call(carobiner::bindr, era_merge["Data.Out"])
 ### Process Data.out file 
 
 d <- data.frame(
+   Time= r1$Time,
    B_code= r1$B.Code,
    country= r1$Country,
    Mean_Season_Prec= ifelse(is.na(r1$Site.MSP.S1) & !is.na(r1$Site.MSP.S2), r1$Site.MSP.S2, r1$Site.MSP.S2),
@@ -32,7 +33,7 @@ d <- data.frame(
    Value= r1$ED.Mean.T,
    variable= gsub(" ", "_",r1$Out.Subind),
    crop= r1$P.Product,
-   variety= r1$ED.Variety,
+   #variety= r1$ED.Variety,
    intercrop= r1$IN.Prods.All,
    product_type= r1$Product.Type,
    version= r1$Version,
@@ -41,6 +42,7 @@ d <- data.frame(
    planting_date_End=  unclass(r1$PD.Plant.End),
    harvest_date= unclass(r1$PD.Harvest.Start),
    harvest_date_End= unclass(r1$PD.Harvest.End),
+   year= r1$Time.Start.Year,
    DAs= r1$PD.Harvest.DAS,
    DAP= r1$PD.Date.DAP,
    rep= r1$ED.Reps,
@@ -51,8 +53,10 @@ d <- data.frame(
    irrigation_fulfullment= r1$I.Strategy,
    irrigated= r1$Irrig,
    plot_area= r1$EX.Plot.Size,
-   variety_type= r1$V.Crop.Practice,
-   land_prep_method= r1$Till.Level.Name,
+   variety_type= r1$V.Type,
+   variety= r1$V.Var,
+   maturity_days= r1$V.Maturity,
+   Till.Level.Name= r1$Till.Level.Name,
    weeding_method= r1$W.Method,
    weeding_times= r1$W.Freq,
    herbicide_product= r1$C.Structure,
@@ -61,10 +65,28 @@ d <- data.frame(
 
    )
 
-#### Adding plant method
+### Adding fertilizer
+rfo <- do.call(carobiner::bindr, era_merge["Fert.Out"])
+
+rf <- r1[, names(rfo)]
+dfert <- data.frame(
+   N_organic= rf$F.NO,
+   P_organic= rf$F.PO,
+   K_organic= rf$F.KO,
+   N_fertilizer= rf$F.NI,
+   P_fertilizer= ifelse(is.na(rf$F.PI)& !is.na(rf$F.P2O5), rf$F.P2O5, rf$F.PI),
+   K_fertilizer= ifelse(is.na(rf$F.KI)& !is.na(rf$F.K2O),  rf$F.K2O, rf$F.KI),
+   fertilizer_type= rf$F.Level.Name2
+)
+
+d <- cbind(d, dfert)
+
+
+#### Process plant.Method file 
 
 ## read plant.Method file 
 rp <- do.call(carobiner::bindr, era_merge["Plant.Method"])
+
 dpM <- data.frame(
    P.Level.Name= rp$P.Level.Name,
    planting_method= rp$Plant.Method,
@@ -107,7 +129,77 @@ dmf <- dmf[, !(names(dmf) %in% names(rpo))]
 ### Removing missing variable in the outcome (value) and B_code 
 dmf <-dmf[!is.na(dmf$variable) & !is.na(dmf$B_code),]
 
+
+
+### Process soil.out file  
+
+r2 <- do.call(carobiner::bindr, era_merge["Soil.Out"])
+
+d1 <- data.frame(
+   
+   location= r2$Site.ID,
+   soil_depth= paste0(r2$Soil.Upper, "-", r2$Soil.Lower),
+   B_code= r2$B.Code,
+   variable= r2$variable,
+   value= r2$value
+)
+
+proc_soil <- function(f){
+   
+   d1 <- d1[d1$B_code==f,]
+   
+   ### Adding  step in the data to facilitate the transformation from long to wide 
+   ds <- d1 %>%
+      group_by(variable) %>%
+      mutate(id = row_number()) %>%
+      ungroup()
+   df <- ds %>%
+      pivot_wider(
+         id_cols = names(ds)[!grepl("variable|value", names(ds))],
+         names_from = variable ,        
+         values_from = value      
+      )
+   
+   return(df)
+   
+}
+
+#### Append the data base on the study (B_code)
+ff <- unique(d1$B_code)
+ds <- lapply(ff, proc_soil)
+ds <- do.call(carobiner::bindr, ds)
+ds$id <- NULL
+names(ds) <- gsub("Soil.", "soil_", names(ds))
+i <- grep("CLY|SLT|SND|soil_AP", names(ds))
+names(ds)[i] <-  c("soil_clay","soil_silt","soil_sand", "soil_P_available")
+
+### merge soil information with dmf
+dmf <- merge(dmf, ds, by=c("location","B_code"), all = TRUE)
+### Removing missing variable in the outcome  
+dmf <- dmf[!is.na(dmf$variable), ] 
+
+### Process tillage file 
+
+r3 <- do.call(carobiner::bindr, era_merge["Till.Out"])
+
+d2 <- data.frame(
+   Till.Level.Name= r3$Till.Level.Name,
+   land_prep_method= ifelse(is.na(r3$T.Method)& !is.na(r3$Till.Other), r3$Till.Other, r3$T.Method),
+   land_prep_implement= r3$T.Mechanization,
+   location= r3$Site.ID,
+   B_code= r3$B.Code,
+   Time= r3$Time,
+   version= r3$Version
+)
+
+## merge d2 and d
+dt <- merge(dmf, d2, by=c("location","B_code", "Till.Level.Name", "Time", "version"), all = TRUE)
+dt <- dt[!is.na(dt$variable), ] 
+dt$Till.Level.Name <- NULL
+
+
 #### Transforming response variable from long into wide format 
+
 proc <- function(f){
    
    d <- d[d$B_code==f,]
@@ -148,45 +240,9 @@ df <- cbind(df[,c(1:36),], df[, c("yield","soil_WHC_sat","soil_SOC", "soil_N","s
 
 
 #### Keep only rows with crop yield value 
-df <- df[!is.na(df$yield),]
+#df <- df[!is.na(df$yield),]
 
 
-### Process soil information  suitable for Soil_data group in carob 
 
-r2 <- do.call(carobiner::bindr, era_merge[3])
-
-d1 <- data.frame(
-   
-   location= r2$Site.ID,
-   soil_depth= paste0(r2$Soil.Upper, "-", r2$Soil.Lower),
-   B_code= r2$B.Code,
-   variable=  gsub(" ", "_",r2$variable),
-   value= r2$value
-)
-
-proc_soil <- function(f){
-   
-   d1 <- d1[d1$B_code==f,]
-   
-### Adding  step in the data to facilitate the transformation from long to wide 
-   ds <- d1 %>%
-      group_by(variable) %>%
-      mutate(id = row_number()) %>%
-      ungroup()
-   df <- ds %>%
-      pivot_wider(
-         id_cols = names(ds)[!grepl("variable|value", names(ds))],
-         names_from = variable ,        
-         values_from = value      
-      )
-   
-   return(df)
-   
-}
-
-#### Append the data base on the study (B_code)
-ff <- unique(d1$B_code)
-ds <- lapply(ff, proc_soil)
-ds <- do.call(carobiner::bindr, ds)
 
 
